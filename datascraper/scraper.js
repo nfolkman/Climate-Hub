e4c3aeeec7730ff327007bbfb9dc39edce13df44
nfolkman <nathanielfolkman@gmail.com>
2022-10-11 00:54:21 -0700

    image scraping fixed (- resolution issues)

--- datascraper/scraper.js
+++ datascraper/scraper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer')
 const URL = 'https://www.dailyclimate.org/impacts/'
 
-
+function wait (ms) {
+   return new Promise(resolve => setTimeout(() => resolve(), ms));
+ }
 
 async function scraper(){
 
@@ -9,10 +11,35 @@ async function scraper(){
 
    const browser = await puppeteer.launch()
    const page = await browser.newPage()
-   await page.goto(URL, {waitUntil: 'domcontentloaded'})
+   await page.goto(URL, {waitUntil: 'load'})
    
    /*** code to take screenshot ***/
    // await page.screenshot({path: 'dailyclimate.png',fullPage: true})
+
+  // Get the height of the rendered page
+  const bodyHandle = await page.$('body');
+  const { height } = await bodyHandle.boundingBox();
+  await bodyHandle.dispose();
+
+  // Scroll one viewport at a time, pausing to let content load
+  const viewportHeight = page.viewport().height;
+  let viewportIncr = 0;
+  while (viewportIncr + viewportHeight < height) {
+    await page.evaluate(_viewportHeight => {
+      window.scrollBy(0, _viewportHeight);
+    }, viewportHeight);
+    await wait(20);
+    viewportIncr = viewportIncr + viewportHeight;
+  }
+
+  // Scroll back to top
+  await page.evaluate(_ => {
+    window.scrollTo(0, 0);
+  });
+
+  // Some extra delay to let images load
+  await wait(100);
+
 
 
    // declare variables
@@ -27,7 +54,7 @@ async function scraper(){
       // target scrape content
       let items = Array.from(document.querySelectorAll('article.page-article'))
 
-      let image = Array.from(document.querySelectorAll('#col-center > div.widget__head > a > img')).map(x => x.src)
+      let image = Array.from(document.querySelectorAll('#col-center > div.widget__head > a > img')).map(x =>  x.src)
 
       let title = Array.from(document.querySelectorAll('.widget__headline-text')).map(x => x.textContent.trim())
       let summary = Array.from(document.querySelectorAll('.body-description')).map(x => x.textContent.trim())
@@ -46,9 +73,22 @@ async function scraper(){
             link: link[i],
             date: date[i],
          })
+         
       }
-      // return list.sort((a, b) => (a.date.length > b.date.length) ? 1 : -1)
-      // return list.sort((a, b) => (a.date.includes('h')) ? -1 : 1)
+      
+      /*** WHY WON'T THIS FUNCTION TO EVALUATE AND EDIT THE IMAGE SOURCE URL ***/
+      // this function makes sure the images will be scraped and displayed in original 416x277 format
+      // for(let i = 0; i<list.length; i++){
+      //    list[image]= list[image].split('&')
+      //    for(let j = 0; j<list[image].length; j++){
+      //       if(list[image][j].includes('width=')) list[image][j] = 'width=416'
+      //       if(list[image][j].includes('height=')) list[image][j] = 'height=277'
+      //    }
+      //    list[image] = list[image].join('&')
+      
+      // }
+
+      
        return list.sort((a,b)=>Date.parse(b.date)-Date.parse(a.date))
       
    })
